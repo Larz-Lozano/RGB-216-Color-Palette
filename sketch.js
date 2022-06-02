605834206d158fb19d5603b769132aebbe6bb05a
Laryze Lozano <95232077+Larz-Lozano@users.noreply.github.com>
2022-06-02 21:49:41 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,21 +3,21 @@ var y=0;
 var r,g,b;
 
 function setup (){
-  createCanvas(1366,240); 
+  createCanvas(1800,240); 
   background (0);
 }
 function draw (){ 
      colorMode (RGB,5);
      for (var i=0; i<6; i++) {
-      for (var j=0; j<1366; j++) {
+      for (var j=0; j<1800; j++) {
         for (var t=0; t<6; t++) {
           //noprotect
      var r=i;
      var g=j;
      var b=t;
      fill (r,g,b);
-    rect (x,y,38,40);
-    x=x+38;
+    rect (x,y,50,40);
+    x=x+50;
   }
    }
    if (x>=width){
